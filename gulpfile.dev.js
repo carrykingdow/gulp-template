3c8341adee1bfc0a92817b289b400543c7e2712d
carrykingdow <jed_shi@163.com>
2019-08-29 17:52:56 +0800

    new readming

--- gulpfile.dev.js
+++ gulpfile.dev.js
@@ -1,96 +1,99 @@
 var gulp = require('gulp'),
-    Config = require('./gulpfile.config'),
-    sass = require('gulp-sass'),
-    livereload = require('gulp-livereload'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    del = require('del'),
-    babel = require('gulp-babel'),
-    fileinclude = require('gulp-file-include'),
-    connect = require('gulp-connect');
+  Config = require('./gulpfile.config'),
+  sass = require('gulp-sass'),
+  livereload = require('gulp-livereload'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  del = require('del'),
+  babel = require('gulp-babel'),
+  fileinclude = require('gulp-file-include'),
+  connect = require('gulp-connect');
 
 // 开发环境下
 function dev() {
-    // 清空dist
-    gulp.task('clean', function(cb) {
-        return del(['./rev', Config.dist_files], cb);
+  // 清空dist
+  gulp.task('clean', function (cb) {
+    return del(['./rev', Config.dist_files], cb);
+  });
+
+  //web服务器
+  gulp.task('webserver', function () {
+    connect.server({
+      livereload: true,
+      directoryListing: true,
     });
+  });
 
-    //web服务器
-    gulp.task('webserver', function() {
-        connect.server({ 
-            livereload: true ,
-            directoryListing: true,
-        });
-    });
+  // js task
+  gulp.task('convertJs:dev', function () {
+    gulp.src(Config.js.src)
+      .pipe(babel({
+        presets: ['env']
+      }))
+      .pipe(gulp.dest(Config.js.dist))
+      .pipe(livereload());
 
-    // js task
-    gulp.task('convertJs:dev', function() {
-        gulp.src(Config.js.src)
-            .pipe(babel({
-                presets: ['env']
-            }))
-            .pipe(gulp.dest(Config.js.dist))
-            .pipe(livereload());
+  });
 
-    });
+  //css task
+  gulp.task('convertCss:dev', function () {
+    //postcss plugin
+    var plugins = [
+      autoprefixer({
+        browsers: ['last 3 version'],
+        cascade: false
+      })
+    ];
+    gulp.src(Config.sass.src)
+      .pipe(sass())
+      .pipe(postcss(plugins)) //带上厂商前缀，对相关css做兼容处理
+      .pipe(gulp.dest(Config.sass.dist))
+      .pipe(livereload());
+  });
 
-    //css task
-    gulp.task('convertCss:dev', function() {
-        //postcss plugin
-        var plugins = [
-            autoprefixer({ browsers: ['last 3 version'], cascade: false })
-        ];
-        gulp.src(Config.sass.src)
-            .pipe(sass())
-            .pipe(postcss(plugins)) //带上厂商前缀，对相关css做兼容处理
-            .pipe(gulp.dest(Config.sass.dist))
-            .pipe(livereload());
-    });
+  //html task
+  gulp.task('convertHtml:dev', function () {
+    gulp.src(Config.html.src)
+      .pipe(fileinclude({
+        prefix: '@@', //变量前缀 @@include
+        basepath: './src/_include', //引用文件路径
+        indent: true //保留文件的缩进
+      }))
+      .pipe(gulp.dest(Config.html.dist))
+      .pipe(livereload());
 
-    //html task
-    gulp.task('convertHtml:dev', function() {
-        gulp.src(Config.html.src)
-            .pipe(fileinclude({
-                prefix: '@@',//变量前缀 @@include
-                basepath: './src/_include',//引用文件路径
-                indent:true//保留文件的缩进
-            }))
-            .pipe(gulp.dest(Config.html.dist))
-            .pipe(livereload());
-
-    });
+  });
 
 
-    //img task
-    gulp.task('convertImg:dev', function() {
-        gulp.src(Config.img.src)
-            .pipe(gulp.dest(Config.img.dist))
-            .pipe(livereload());
+  //img task
+  gulp.task('convertImg:dev', function () {
+    gulp.src(Config.img.src)
+      .pipe(gulp.dest(Config.img.dist))
+      .pipe(livereload());
 
-    })
-
-
-    // copy lib下的所有文件
-    gulp.task('copylib:dev', function() {
-         gulp.src(Config.lib.src)
-            .pipe(gulp.dest(Config.lib.dist))
-            .pipe(livereload());
-
-    })
+  })
 
 
-    gulp.task('dev', ['webserver', 'convertHtml:dev', 'convertJs:dev', 'convertImg:dev', 'copylib:dev', 'convertCss:dev'], function() {
-        livereload.listen();
-        gulp.watch(Config.js.src, ['convertJs:dev']); //监听js文件
-        gulp.watch(Config.sass.src, ['convertCss:dev']); //监听 css
-        gulp.watch([Config.html.src, './src/_include/*'], ['convertHtml:dev']); //监听html
-        gulp.watch(Config.img.src, ['convertImg:dev']); //监听img
-        gulp.watch(Config.lib.src, ['copylib:dev']); //监听lib
-        console.log("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀")       
-        console.log("             dev打包成功！                ")       
-        console.log("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀")       
-    });
+  // copy lib下的所有文件
+  gulp.task('copylib:dev', function () {
+    gulp.src(Config.lib.src)
+      .pipe(gulp.dest(Config.lib.dist))
+      .pipe(livereload());
+
+  })
+
+
+  gulp.task('dev', ['webserver', 'convertHtml:dev', 'convertJs:dev', 'convertImg:dev', 'copylib:dev', 'convertCss:dev'], function () {
+    livereload.listen();
+    gulp.watch(Config.js.src, ['convertJs:dev']); //监听js文件
+    gulp.watch(Config.sass.src, ['convertCss:dev']); //监听 css
+    gulp.watch([Config.html.src, './src/_include/*'], ['convertHtml:dev']); //监听html
+    gulp.watch(Config.img.src, ['convertImg:dev']); //监听img
+    gulp.watch(Config.lib.src, ['copylib:dev']); //监听lib
+    console.log("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀")
+    console.log("             dev打包成功！                ")
+    console.log("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀")
+  });
 
 }
 
--- gulpfile.prod.js
+++ gulpfile.prod.js
@@ -1,107 +1,117 @@
 var gulp = require('gulp'),
     Config = require('./gulpfile.config.js'),
     uglify = require('gulp-uglify'),
-    // rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     minifyCss = require('gulp-clean-css'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
-    // concat = require('gulp-concat'),
     del = require('del'),
     babel = require('gulp-babel'),
-    // connect = require('gulp-connect'),
     rev = require('gulp-rev'),
     revCollector = require('gulp-rev-collector'),
-    // gulpSequence = require('gulp-sequence'),
     htmlmin = require('gulp-htmlmin'),
     fileinclude = require('gulp-file-include')
     imagemin = require('gulp-imagemin');
 // 开发环境下
 function prod() {
 
-    // 清空dist
-    gulp.task('clean', function(cb) {
-        return del(['./rev', Config.dist_files], cb);
-    });
-    // js task
-    gulp.task('convertJs', function() {
-        return gulp.src(Config.js.src)
-            .pipe(babel({
-                presets: ['env']
-            }))
-            .pipe(uglify()) //压缩
-            .pipe(rev())
-            .pipe(gulp.dest(Config.js.dist))
-            .pipe(rev.manifest())
-            .pipe(gulp.dest('./rev/js'))
+  // 清空dist
+  gulp.task('clean', function (cb) {
+    return del(['./rev', Config.dist_files], cb);
+  });
+  // js task
+  gulp.task('convertJs', function () {
+    return gulp.src(Config.js.src)
+      .pipe(babel({
+        presets: ['env']
+      }))
+      .pipe(uglify()) //压缩
+      .pipe(rev())
+      .pipe(gulp.dest(Config.js.dist))
+      .pipe(rev.manifest())
+      .pipe(gulp.dest('./rev/js'))
 
-    });
+  });
 
-    //css task
-    gulp.task('convertCss', function() {
-        //postcss plugin
-        var plugins = [
-            autoprefixer({ browsers: ['last 3 version'], cascade: false })
-        ];
-        return gulp.src(Config.sass.src)
-            .pipe(sass())
-            .pipe(postcss(plugins)) //带上厂商前缀，对相关css做兼容处理
-            .pipe(minifyCss())
-            .pipe(rev())
-            .pipe(gulp.dest(Config.sass.dist))
-            .pipe(rev.manifest())
-            .pipe(gulp.dest('./rev/css'))
-    });
-
-
-
-    //html task
-    gulp.task('convertHtml', ['convertJs', 'convertCss'], function() {
-        return gulp.src(["./rev/**/*.json", Config.html.src])
-
-            //执行文件内引用名的替换
-            .pipe(revCollector({
-                replaceReved: true
-            }))
-            .pipe(fileinclude({
-                prefix: '@@',//变量前缀 @@include
-                basepath: './src/_include',//引用文件路径
-                indent:true//保留文件的缩进
-            }))
-            .pipe(htmlmin({ collapseWhitespace: true }))
-            .pipe(gulp.dest(Config.html.dist))
-    });
+  //css task
+  gulp.task('convertCss', function () {
+    //postcss plugin
+    var plugins = [
+      autoprefixer({
+        browsers: ['last 3 version'],
+        cascade: false
+      })
+    ];
+    return gulp.src(Config.sass.src)
+      .pipe(sass())
+      .pipe(postcss(plugins)) //带上厂商前缀，对相关css做兼容处理
+      .pipe(minifyCss())
+      .pipe(rev())
+      .pipe(gulp.dest(Config.sass.dist))
+      .pipe(rev.manifest())
+      .pipe(gulp.dest('./rev/css'))
+  });
 
 
-    //img task
-    gulp.task('convertImg', function() {
-        gulp.src(Config.img.src)
-            .pipe(imagemin([
-                imagemin.gifsicle({ interlaced: true }),
-                imagemin.jpegtran({ progressive: true }),
-                imagemin.optipng({ optimizationLevel: 5 }),
-                imagemin.svgo({
-                    plugins: [
-                        { removeViewBox: true },
-                        { cleanupIDs: false }
-                    ]
-                })
-            ]))
-            .pipe(gulp.dest(Config.img.dist))
-    })
+
+  //html task
+  gulp.task('convertHtml', ['convertJs', 'convertCss'], function () {
+    return gulp.src(["./rev/**/*.json", Config.html.src])
+
+      //执行文件内引用名的替换
+      .pipe(revCollector({
+        replaceReved: true
+      }))
+      .pipe(fileinclude({
+        prefix: '@@', //变量前缀 @@include
+        basepath: './src/_include', //引用文件路径
+        indent: true //保留文件的缩进
+      }))
+      .pipe(htmlmin({
+        collapseWhitespace: true
+      }))
+      .pipe(gulp.dest(Config.html.dist))
+  });
 
 
-    // copy lib下的所有文件
-    gulp.task('copylib', function() {
-        gulp.src(Config.lib.src)
-        .pipe(gulp.dest(Config.lib.dist))
-    })
-    gulp.task('build', ['convertJs', 'convertImg', 'copylib', 'convertCss'], function() {
-        gulp.start('convertHtml');
-        console.log("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀")       
-        console.log("             build 打包成功！                ")       
-        console.log("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀")    
-    });
+  //img task
+  gulp.task('convertImg', function () {
+    gulp.src(Config.img.src)
+      .pipe(imagemin([
+        imagemin.gifsicle({
+          interlaced: true
+        }),
+        imagemin.jpegtran({
+          progressive: true
+        }),
+        imagemin.optipng({
+          optimizationLevel: 5
+        }),
+        imagemin.svgo({
+          plugins: [{
+              removeViewBox: true
+            },
+            {
+              cleanupIDs: false
+            }
+          ]
+        })
+      ]))
+      .pipe(gulp.dest(Config.img.dist))
+  })
+
+
+  // copy lib下的所有文件
+  gulp.task('copylib', function () {
+    gulp.src(Config.lib.src)
+      .pipe(gulp.dest(Config.lib.dist))
+  })
+  gulp.task('build', ['convertJs', 'convertImg', 'copylib', 'convertCss'], function () {
+    gulp.start('convertHtml');
+    console.log("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀")
+    console.log("             build 打包成功！                ")
+    console.log("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀")
+  });
 
 }
 
